4f93dc3badbb656503c73d1cd378d2da820dc6f1
Stephane Poirier <stephane.poirier@oifii.org>
2016-12-13 02:13:30 -0500

    license and readme

--- spimidiprogramchange.cpp
+++ spimidiprogramchange.cpp
@@ -1,4 +1,28 @@
 /*
+ * Copyright (c) 2010-2016 Stephane Poirier
+ *
+ * stephane.poirier@oifii.org
+ *
+ * Stephane Poirier
+ * 3532 rue Ste-Famille, #3
+ * Montreal, QC, H2X 2L1
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/*
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //the midinote.c, midiprogramchange.c and midicontinuouscontroller.c functions are portmidi equivalents of
 //the midinote.jar, midiprogramchange.jar and midicontinuouscontroller.jar java class archives which are java sound midi equivalents of
@@ -28,7 +52,6 @@
 //
 //2014may04, version 0.2, spi, renamed spimidiprogramchange.c
 //
-//copyright 2012, stephane.poirier@nakedsoftware.org
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 */
 
